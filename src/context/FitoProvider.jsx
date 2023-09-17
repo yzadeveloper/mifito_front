061b0dbee54a5a9595727feb0e037e45e504ada0
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-09-18 00:10:50 +0200

    .env

--- src/context/FitoProvider.jsx
+++ src/context/FitoProvider.jsx
@@ -1,16 +1,34 @@
+import { useEffect } from "react";
 import { createContext, useState } from "react"
-import { categories as categoriesDB } from "../data/categories"
+
+import axios from "axios";
 
 const FitoContext = createContext();
 
 
 const FitoProvider = ({children}) => {
 
-    const [categories, setCategories] = useState(categoriesDB);
-    const [categoryActive, setcategoryActive] =useState(categories[0]);
+    const [categories, setCategories] = useState([]);
+    const [categoryActive, setcategoryActive] =useState({});
+
+    const obtainCategories = async () => {
+        try {
+            
+            const {data} = await axios(`${import.meta.env.VITE_API_URL}/api/categories`)
+            setCategories(data.data)
+            setcategoryActive(data.data[0])
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    useEffect(() => {
+        obtainCategories();
+    }, [])
     
-    const handleClickCategory = id => {
-        const category = categories.filter(category => category.id_category === id)[0]
+    const handleClickCategory = id_category => {
+        const category = categories.filter(category => category.id_category === id_category)[0]
         setcategoryActive(category)
     }
    
